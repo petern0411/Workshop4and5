c5f45638c73216a7e87b30676761a5dec076e0a1
Peter Nguyen <Peternguyen0411@gmail.com>
2016-10-25 18:27:17 -0400

    fb4

--- app/database.js
+++ app/database.js
@@ -39,7 +39,6 @@ var initialData = {
         2, 3
       ],
       // The type and contents of this feed item. This item happens to be a status
-
       // update.
       "type": "statusUpdate",
       "contents": {
@@ -85,7 +84,6 @@ var initialData = {
     "2": {
       "_id": 2,
       "contents": []
-
     },
     "1": {
       "_id": 1,
@@ -93,3 +91,80 @@ var initialData = {
     }
   }
 };
+
+var data = JSON.parse(localStorage.getItem('facebook_data'));
+if (data === null) {
+  data = JSONClone(initialData);
+}
+
+/**
+* A dumb cloning routing. Serializes a JSON object as a string, then
+* deserializes it.
+*/
+function JSONClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+/**
+* Emulates reading a "document" from a NoSQL database.
+* Doesn't do any tricky document joins, as we will cover that in the latter
+* half of the course. :)
+*/
+export function readDocument(collection, id) {
+  // Clone the data. We do this to model a database, where you receive a
+  // *copy* of an object and not the object itself.
+  return JSONClone(data[collection][id]);
+}
+
+/**
+* Emulates writing a "document" to a NoSQL database.
+*/
+export function writeDocument(collection, changedDocument) {
+  var id = changedDocument._id;
+  // Store a copy of the object into the database. Models a database's behavior.
+  data[collection][id] = JSONClone(changedDocument);
+  // Update our 'database'.
+  localStorage.setItem('facebook_data', JSON.stringify(data));
+}
+
+/**
+* Adds a new document to the NoSQL database.
+*/
+export function addDocument(collectionName, newDoc) {
+  var collection = data[collectionName];
+  var nextId = Object.keys(collection).length;
+  while (collection[nextId]) {
+    nextId++;
+  }
+  newDoc._id = nextId;
+  writeDocument(collectionName, newDoc);
+  return newDoc;
+}
+
+/**
+* Reset our browser-local database.
+*/
+export function resetDatabase() {
+  localStorage.setItem('facebook_data', JSON.stringify(initialData));
+  data = JSONClone(initialData);
+}
+
+/**
+* Reset database button.
+*/
+class ResetDatabase extends React.Component {
+  render() {
+    return (
+      <button className="btn btn-default" type="button" onClick={() => {
+          resetDatabase();
+          window.alert("Database reset! Refreshing the page now...");
+          document.location.reload(false);
+        }}>Reset Mock DB</button>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <ResetDatabase />,
+    document.getElementById('fb-db-reset')
+  );
